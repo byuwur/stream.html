e7eb98d29296b54c52c1ae62bd843b1a69206e8c
[Mateus] byUwUr <atrujillomateus@gmail.com>
2025-03-22 00:32:20 -0500

    Format.

--- _streamlabs_chat/script.js
+++ _streamlabs_chat/script.js
@@ -1,8 +1,18 @@
-document.addEventListener("onLoad", function (obj) {
-	$('<style id="newStyle">.shiftGlitch:after {animation: shift ' + { glitchSpeed } + "s " + { repeatAmount } + " linear alternate-reverse;}.shiftGlitch:before{animation: shift-2 " + { glitchSpeed } * 3 + "s " + { repeatAmount } + " linear alternate-reverse;}</style>").appendTo("head");
+document.addEventListener("onLoad", function () {
+	$(
+		'<style id="newStyle">.shiftGlitch:after {animation: shift ' +
+			{glitchSpeed} +
+			"s " +
+			{repeatAmount} +
+			" linear alternate-reverse;}.shiftGlitch:before{animation: shift-2 " +
+			{glitchSpeed} * 3 +
+			"s " +
+			{repeatAmount} +
+			" linear alternate-reverse;}</style>"
+	).appendTo("head");
 });
 
-document.addEventListener("onEventReceived", function (obj) {
+document.addEventListener("onEventReceived", function () {
 	const childrenCount = $("#log .line").length;
-	if (childrenCount >= { maxLines } + 1) $("#log .line:first-child").remove();
+	if (childrenCount >= {maxLines} + 1) $("#log .line:first-child").remove();
 });
